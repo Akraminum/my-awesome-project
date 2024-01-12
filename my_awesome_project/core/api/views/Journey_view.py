aed6748b20f385b66e33a866ab29fec7a4a2899b
akraminful <akraminum@codful.com>
2024-01-12 02:13:32 +0200

    .

--- my_awesome_project/core/api/views/Journey_view.py
+++ my_awesome_project/core/api/views/Journey_view.py
@@ -1,50 +1,71 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
-from django.db.models import Count
+from django.db.models import Prefetch, Q, Count
+
+from my_awesome_project.core.models import Journey, Goal
+
+from my_awesome_project.core.managers.JourneyManager import JourneyManager
 
 from my_awesome_project.core.api.serializers.journey.create import JourneyCreateSerializer
 from my_awesome_project.core.api.serializers.journey.detail import JourneyDetailSerializer
 from my_awesome_project.core.api.serializers.journey.list import JourneyListSerializer
 from my_awesome_project.core.api.serializers.journey.update import JourneyUpdateSerializer
 
-from my_awesome_project.core.models import Journey, Goal
-from django.db.models import Prefetch, Q
 
-    
 class JourneyViewSet(viewsets.ModelViewSet):
-    filter_backends = [OrderingFilter]
-    ordering_fields = ['name']
-    ordering = ['name']
-    
-    # TODO: will be moved to model manager
     queryset_map = {
-        "default": Journey.objects.all(),
-        "list": Journey.objects.all().annotate(goals_count=Count('goals')),
-        "retrieve": Journey.objects.all().prefetch_related(
-            Prefetch(
-                'goals', 
-                queryset=Goal.objects.all().annotate(
-                    total_targets_count=Count('targets'),
-                    achieved_targets_count=Count('targets', filter=Q(targets__is_achieved=True))
-                )
-            )
-        )
-    } 
-    def get_queryset(self):
-        return self.queryset_map.get(
-            self.action,
-            self.queryset_map.get("default"))
-        
-    
+        'list': Journey.objects.list,
+        'retrieve': Journey.objects.retrieve,
+        'create': Journey.objects.all,
+        'update': Journey.objects.all,
+        'destroy': Journey.objects.all,
+        'goals': Journey.objects.all,
+    }
     serializer_map = {
         'list': JourneyListSerializer,
+        'retrieve': JourneyDetailSerializer,
         'create': JourneyCreateSerializer,
         'update': JourneyUpdateSerializer,
-        'partial_update': JourneyUpdateSerializer,
-        'retrieve': JourneyDetailSerializer,
+        'destroy': JourneyDetailSerializer,
+        'goals': JourneyDetailSerializer,
     }
-    def get_serializer_class(self):
-        return self.serializer_map.get(
-            self.action, 
-            self.serializer_map.get("default")) 
+    filter_backends = (OrderingFilter,)
+    ordering_fields = ('name',)
+    ordering = ('name',)
+    lookup_field = 'slug'
+    lookup_url_kwarg = 'slug'   
+    http_method_names = ['get', 'post', 'put', 'delete']
+    permission_classes = []
+    authentication_classes = []
+
+    def get_queryset(self):
+        return self.queryset_map.get(self.action, self.queryset_map['list'])()
     
+    def get_serializer_class(self):
+        return self.serializer_map.get(self.action, self.serializer_map['list'])
+    
+    def perform_create(self, serializer):
+        serializer.save()
+        serializer = self.get_serializer_class("retrieve")(instance=serializer.instance)
+        return serializer
+    
+    def perform_update(self, serializer):
+        serializer.save()
+        serializer = self.get_serializer_class("retrieve")(instance=serializer.instance)
+        return serializer
+    
+    @action(detail=True, methods=['get'])
+    def goals(self, request, slug=None):
+        journey = self.get_object()
+        goals = journey.goals.all()
+        serializer = self.get_serializer(goals, many=True)
+        return Response(serializer.data)
+    
+    
+    
+    
+
+
+
--- my_awesome_project/core/models/journey.py
+++ my_awesome_project/core/models/journey.py
@@ -1,10 +1,12 @@
 from django.db import models
+from my_awesome_project.core.managers.JourneyManager import JourneyManager
 from model_utils.models import TimeStampedModel
 
 
-# Create your models here.
 class Journey(TimeStampedModel):
     name = models.CharField(max_length=255)
+
+    objects = JourneyManager()
 
     def __str__(self):
         return self.name
