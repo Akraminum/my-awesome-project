9c8c134906ae5999c8307f0c2a06918212f8f3fb
Akraminful <akraminum@codful.com>
2024-01-16 08:01:02 +0200

    .

--- my_awesome_project/core/models/journey.py
+++ my_awesome_project/core/models/journey.py
@@ -5,7 +5,5 @@ from model_utils.models import TimeStampedModel
 class Journey(TimeStampedModel):
     name = models.CharField(max_length=255)
 
-    # objects = JourneyManager()
-
     def __str__(self):
         return self.name
--- my_awesome_project/users/models.py
+++ my_awesome_project/users/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    title = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to="users/avatar", default="users/avatar/default.png"  )
+    birth_date = models.DateField(null=True)
+    
+    class Meta:
+        app_label = "users"
+        db_table = "auth_user"
